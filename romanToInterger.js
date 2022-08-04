ed56d3358e844bbc1cb5607bc4b3b7d2b8d77f37
isurojit <iamsurojit@hotmail.com>
2022-08-04 21:31:00 +0530

    completed romanToInterger.js

--- romanToInterger.js
+++ romanToInterger.js
@@ -28,3 +28,28 @@ Input: s = "III"
 Output: 3
 Explanation: III = 3.
 */
+/*
+ * @param {string} s
+ * @return {number}
+ */
+
+const romanToInt = (s) => {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const curr = values[s[i]];
+    const next = values[s[i + 1]];
+    result += curr < next ? -curr : curr;
+  }
+  return result;
+};
+
+console.log(romanToInt("MCMXCIV"));
